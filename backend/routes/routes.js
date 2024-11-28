3d44c166966a6fc6e970511993b15a140bae7524
Ziyad Khan <ziyadk0212@gmail.com>
2024-11-28 04:42:17 -0500

    Added header and footer as well as 'add workouts' page

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -5,4 +5,8 @@ router.get("/",(req,res) => {
     res.render('index', {title:"Home Page"});
 });
 
+router.get("/add", (req,res) => {
+    res.render("add_workout", {title:"Add Workout"});
+})
+
 module.exports = router;
